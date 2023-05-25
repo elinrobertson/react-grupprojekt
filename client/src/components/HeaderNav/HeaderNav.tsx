349fb016c9b373a2f76b57bd033d0d6185ed64b3
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-05-25 15:05:58 +0200

    header style and layout

---
+++ client/src/components/HeaderNav/HeaderNav.tsx
@@ -0,0 +1,42 @@
+import "./HeaderNav.css"
+import 'antd/dist/reset.css';
+import { NavLink } from "react-router-dom"
+import { Menu } from "antd";
+
+
+const menuItems = [{
+    label: "Nyheter",
+    key: "news",
+
+},
+{   
+     label: (<NavLink to={"produkter"}>Produkter </NavLink>),
+key: "products",
+},
+{   
+    label: "Kontakt",
+key: "contact",
+},
+{   
+    label: "Om oss",
+key: "about",
+},
+]
+
+const HeaderNav = () => {
+  return (
+
+<Menu className="headerNav"  mode="horizontal" items={menuItems}></Menu>
+
+    // <div className="headerNav">
+    //     <ul>
+    //     <li><NavLink to="/">Produkter</NavLink></li>
+    //     <li><NavLink to="/about">Om oss</NavLink></li>
+    //    <li><NavLink to="/contact">Contakt</NavLink></li>
+    //     <li><NavLink to="/blalalal">Blalala</NavLink></li>
+    //     </ul>
+    // </div>
+  )
+}
+
+export default HeaderNav
