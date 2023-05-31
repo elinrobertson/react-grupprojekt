1fbd72b3b5dc7d896c020ad6b047ad30d65228c4
Sara Agnestrand <sara@agnestrand.net>
2023-05-31 13:03:54 +0200

    link to product ProductDetail

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import "./ProductCard.css";
 import { Product } from "../ProductList/ProductList";
 import { AntDesignButton } from "../AntDesign/AntDesign";
 import { NavLink } from "react-router-dom";
+import ProductDetail  from '../ProductDetail/ProductDetail';
 
 interface ProductCardProps {
   products: Product;
@@ -13,7 +14,7 @@ function ProductCard({ products }: ProductCardProps) {
   return (
     <div className="productCard-div">
       <NavLink to={"/" + products._id}> 
-      <div className="productCard-link">
+      <div className="productCard-link" onClick ={ () => { ProductDetail();}} >
         <img src={products.image} alt={products.title} />
         <div className="productCard-info">
           <h3>{products.title}</h3>
