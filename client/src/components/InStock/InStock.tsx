71619b7434ad98b4e60815299ba8eb9c51ea8983
Sara Agnestrand <sara@agnestrand.net>
2023-06-02 11:13:26 +0200

    product detail done

--- client/src/components/InStock/InStock.tsx
+++ client/src/components/InStock/InStock.tsx
@@ -5,25 +5,69 @@ interface InStockProps {
 }
 
 
-//const[ product, setInStock]= useState<Product|undefined>();
+// //const[ product, setInStock]= useState<Product|undefined>();
 
 
-function InStock({ product }: InStockProps) {
+// function InStock({ product }: InStockProps) {
 
-  let stockMessage: string;
-    console.log(product.inStock);
+//   let stockMessage: string;
+//     console.log(product.inStock);
 
 
-  if (product.inStock === 0) {
-    stockMessage = 'Ej i lager';
-  } else if (product.inStock <5) {
-    stockMessage = 'Fåtal i lager';
-  } else {
-    stockMessage = 'I lager';
-  }
+//   if (product.inStock === 0) {
+//     stockMessage = 'Ej i lager';
+//   } else if (product.inStock <5) {
+//     stockMessage = 'Fåtal i lager';
+//   } else {
+//     stockMessage = 'I lager';
+//   }
 
-  return (<div>{stockMessage}</div>);
-}
+//   return (<div>{stockMessage}</div>);
+// }
 
+
+// export default InStock;
+
+
+// interface InStockProps {
+//   product: {
+//     inStock: number;
+//   };
+// }
+
+const InStock = ({ product }: InStockProps) => {
+  const getStockMessage = () => {
+    let stockMessage: string;
+
+    if (product.inStock === 0) {
+      stockMessage = 'Ej i lager';
+    } else if (product.inStock < 5) {
+      stockMessage = 'Fåtal i lager';
+    } else {
+      stockMessage = 'I lager';
+    }
+
+    return stockMessage;
+  };
+
+  const getStockMessageColor = () => {
+    if (product.inStock === 0) {
+      return 'rgb(219, 107, 95)';
+    } else if (product.inStock < 5) {
+      return 'rgb(223, 160, 77)';
+    } else {
+      return '#809F8A';
+    }
+  };
+
+  const stockMessageStyle = {
+    color: getStockMessageColor(),
+    fontSize: '14px'
+  };
+
+  return <div style={stockMessageStyle}>{getStockMessage()}</div>;
+};
 
 export default InStock;
+
+
