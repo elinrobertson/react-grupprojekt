f962898df54b623ee7d1e663807a3a05fa1154f7
heinanX <larrii@live.se>
2023-06-05 21:48:04 +0200

    fixed values

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,14 +3,28 @@ import { Link } from "react-router-dom"
 import HeaderNav from "../HeaderNav/HeaderNav.tsx"
 import UserDropDown from "../AntDesign/UserDropdown/UserDropdown.tsx";
 import { ShoppingCartOutlined } from "@ant-design/icons"
+import { useContext } from "react"
+import { CartContext } from "../../context/CartContext.tsx";
 
 function Header() {
+  // GETS CART CONTEXT
+  const {currentCart} = useContext(CartContext)
+  //STORES A CONDITIONAL INSIDE VARIABLE NUM
+  const num = currentCart.totalQuantity === 0 ? null : currentCart.totalQuantity
+  //STORES A CONDITIONAL INSIDE VARIABLE PRINTCARTITEMS = TRUE=> DIV : FALSE=> NULL
+  const printCartItems = num ?
+    <div className="currentItemsInCart-div">
+      <p style={{ margin: '0', fontSize: '12px', color: 'white'}}>{num}</p>
+    </div> : null;
 
   return (
     <header>
       <div className="header-icons">
         <UserDropDown />
-        <ShoppingCartOutlined />
+        <div  style={{position: 'relative', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+          <ShoppingCartOutlined />
+          {printCartItems}
+        </div>
       </div>
       <Link to="/">
         <div className="logo">
