fc8257a26b70dc4428f308fcbef69946659266f4
Elin Robertson <robertsonelin@gmail.com>
2023-06-09 12:30:35 +0200

    form checkout

--- client/src/components/AddressForm/AddressForm.tsx
+++ client/src/components/AddressForm/AddressForm.tsx
@@ -1,20 +1,7 @@
-import type { CascaderProps } from 'antd';
-import {
-  AutoComplete,
-  Button,
-  Cascader,
-  Checkbox,
-  Col,
-  Form,
-  Input,
-  InputNumber,
-  Row,
-  Select,
-} from 'antd';
-import { useState } from 'react';
-
-
-
+import { Checkbox, Form, Input } from "antd";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import "./AddressForm.css";
 
 const formItemLayout = {
   labelCol: {
@@ -44,127 +31,104 @@ const AddressForm = () => {
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
+    console.log("Received values of form: ", values);
   };
 
-  const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
-
+  const { loggedinUser } = useContext(UserContext);
 
   return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      style={{ maxWidth: 600 }}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-post"
-        rules={[
-          { type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <div className="form-div">
+        <div className="userData-div">
+          <p>
+            {loggedinUser?.firstName} {loggedinUser?.lastName}
+          </p>
+          <p>{loggedinUser?.email}</p>
+        </div>
+        <Form
+          {...formItemLayout}
+          form={form}
+          name="register"
+          onFinish={onFinish}
+          style={{ maxWidth: 600 }}
+          scrollToFirstError
+        >
+          <Form.Item
+            name="street"
+            label="Adress"
+            rules={[
+              {
+                required: true,
+                message: "Var god fyll i gata!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-    
+          <Form.Item
+            name="zipcode"
+            label="Postnummer"
+            rules={[
+              {
+                required: true,
+                message: "Var god fyll i postnummer!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        name="firstname"
-        label="Förnamn"
-        rules={[{ required: true, message: 'Var god fyll i förnamn!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
+          <Form.Item
+            name="city"
+            label="Ort"
+            rules={[
+              {
+                required: true,
+                message: "Var god fyll i stad!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        name="lastname"
-        label="Efternamn"
-        rules={[{ required: true, message: 'Var god fyll  efternamn!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
+          <Form.Item
+            name="country"
+            label="Land"
+            rules={[
+              {
+                required: true,
+                message: "Var god fyll i land!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        name="street"
-        label="Adress"
-        rules={[{ required: true, message: 'Var god fyll i gata!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="zipcode"
-        label="Postnummer"
-        rules={[{ required: true, message: 'Var god fyll i postnummer!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="city"
-        label="Ort"
-        rules={[{ required: true, message: 'Var god fyll i stad!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="country"
-        label="Land"
-        rules={[{ required: true, message: 'Var god fyll i land!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
-
-      {/* <Form.Item
-        name="address"
-        label="Adress"
-        rules={[
-          { type: 'array', required: true, message: 'Var god fyll i din adress!' },
-        ]}
-      >
-        <Cascader options={residences} />
-      </Form.Item> */}
-
-      {/* <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[{ required: true, message: 'Var god fyll i ditt telefonnummer!' }]}
-      >
-        <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-      </Form.Item> */}
-
-
-      <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[
-          {
-            validator: (_, value) =>
-              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-          },
-        ]}
-        {...tailFormItemLayout}
-      >
-        <Checkbox>
-          Jag godkänner <a href="">köpvillkoren</a>
-        </Checkbox>
-      </Form.Item>
-      {/* <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item> */}
-    </Form>
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value
+                    ? Promise.resolve()
+                    : Promise.reject(new Error("Acceptera köpvillkoren")),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              Jag godkänner <a href="">köpvillkoren</a>
+            </Checkbox>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   );
 };
 
